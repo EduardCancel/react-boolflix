7fd56cc4b32e8fb78e459932f8d8af15cd328d44
IlTuoNome <laTuaEmail@example.com>
2025-04-07 12:04:45 +0200

    enhance GlobalContext to support TV show search and include poster paths in results

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useState, useContext } from "react";
 
+
 const GlobalContext = createContext();
 
 const api_key = import.meta.env.VITE_MOVIE_DB_API_KEY;
@@ -8,32 +9,36 @@ export default function GlobalProvider({ children }) {
     const [searchText, setSearchText] = useState("");
     const [searchResults, setSearchResults] = useState([]);
 
+
     const handleSearch = () => {
         const movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${searchText}`;
         const tvUrl = `https://api.themoviedb.org/3/search/tv?api_key=${api_key}&query=${searchText}`;
 
-        // Primo fetch per i film
+
         fetch(movieUrl)
             .then((res) => res.json())
             .then((movieData) => {
+
 
                 const movies = movieData.results.map((item) => ({
                     id: item.id,
                     title: item.title,
                     original_language: item.original_language,
-                    vote_count: item.vote_count
+                    vote_count: item.vote_count,
+                    poster_path: item.poster_path
                 }));
 
-                // Secondo fetch per le serie TV
+
                 fetch(tvUrl)
                     .then((res) => res.json())
                     .then((tvData) => {
-                        // Mappa i dati delle serie TV
+
                         const tvShows = tvData.results.map((item) => ({
                             id: item.id,
                             title: item.name,
                             original_language: item.original_language,
-                            vote_count: item.vote_count
+                            vote_count: item.vote_count,
+                            poster_path: item.poster_path
                         }));
 
 
@@ -55,6 +60,7 @@ export default function GlobalProvider({ children }) {
     );
 }
 
+// Funzione per usare il contesto
 function useGlobalContext() {
     return useContext(GlobalContext);
 }
